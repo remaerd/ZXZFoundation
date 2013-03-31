2ddc45f36c83867937e3a472d48e26f6f2361ca4
Sean Cheng <me@remaerd.com>
2013-03-31 13:17:56 +0800

    Initial Commit

---
+++ SCFoundation/SCFoundation.h
@@ -0,0 +1,13 @@
+//
+//  SCFoundation.h
+//  SCFoundation
+//
+//  Created by Sean Cheng on 3/31/13.
+//  Copyright (c) 2013 Extremely Limited. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SCFoundation : NSObject
+
+@end
